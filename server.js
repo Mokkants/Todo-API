d87b10e8249f9322c31073faa9df2e686df58b0d
Bence Ludmany <ludmany.bence@gmail.com>
2017-03-22 22:52:58 +0100

    Added todos and todos/:id

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 var express=require("express");
 var app=express();
 var port=process.env.PORT || 3000;
-
+var todos=[
+{
+  id:1,
+  description:"Meet mom for lunch",
+  completed:false
+},
+{
+  id:2,
+  description:"Go to market",
+  completed:false
+},
+{
+  id:3,
+  description:"Make dinner",
+  completed:true
+}
+];
 app.get("/",function(req,res){
   res.send("Todo API Root");
+});
+
+app.get("/todos",function(req,res){
+  res.json(todos);
+});
+
+app.get("/todos/:id",function(req,res){
+  var todoID=parseInt(req.params.id,10);
+  todos.forEach(function(todo){
+    if(todo.id===todoID){
+      res.json(todo);
+    }
+  });
+  res.status(404).send();
 });
 
 app.listen(port,function(){
