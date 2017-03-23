53b0758fbbbb463036c74694bd8916f3de6d912f
Bence Ludmany <ludmany.bence@gmail.com>
2017-03-23 18:39:57 +0100

    Improved code with underscore.js

--- server.js
+++ server.js
@@ -68,6 +68,33 @@ else{
 
 });
 
+//Updating a ToDo
+app.put("/todos/:id",function(req,res){
+  var body=_.pick(req.body,"description","completed");
+  var validAttributes={};
+  var todoID=parseInt(req.params.id,10);
+  var matchedTodo=_.findWhere(todos,{id:todoID});
+
+  if(!matchedTodo){
+    return res.status(404).send();
+  }
+
+  if(body.hasOwnProperty("completed") && _.isBoolean(body.completed)){
+    validAttributes.completed=body.completed;
+  }else if(body.hasOwnProperty("completed")){
+    return res.status(400).send();
+  }
+
+if(body.hasOwnProperty("description") && _.isString(body.description) && body.description.trim().length>0){
+  validAttributes.description=body.description;
+}else if(body.hasOwnProperty("description")){
+  return res.status(400).send();
+}
+
+_.extend(matchedTodo, validAttributes);
+res.json(matchedTodo);
+});
+
 
 app.listen(port,function(){
   console.log("Express listening on port "+port+"!");
